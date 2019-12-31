9dd0bd6baaf53e369031552e9fb3126b7cef6a63
Winaer <2285206109@qq.com>
2019-12-31 19:17:32 +0800

    see

--- for_test.c
+++ for_test.c
@@ -43,6 +43,35 @@ void test1()
 
 }
 
+void test2()
+{
+
+    // double pi = 3.1415926;
+    // double m[20];
+    int a[5][4];
+
+    int i;
+    int j;
+
+
+    for ( i = 0; i < 5; i++)
+    {
+       for ( j = 0; j < 4; j++)
+       {
+          a[i][j] = 5*i + j;
+       }
+       
+    }
+    printf("a = %p\n", a);
+    printf("a+1 = %p\n", a+1);
+    printf("*(a+1)+1 =   %p     &(a[1][1]) = %p\n", *(a+1)+1, &(a[1][1]));
+    printf("&a[1]+1 =    %p     &(a[2][0])=  %p\n", &a[1]+1, &(a[2][0]));
+    printf("*((a+2)+1) = %p     &(a[3][0]) = %p\n", *((a+2)+1), &(a[3][0]) );
+    printf("this is complex logic!\n");
+
+
+}
+
 
 void test3()
 {
@@ -137,7 +166,7 @@ void test6()
 int main(void)
 {
     test1();
-
+    test2();
     test3();
     test4();
     test5();
